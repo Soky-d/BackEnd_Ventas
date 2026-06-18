c8fe0841c5c9a164a39f345d09d4e44fc0c991ed
DanteSolis <dante.solis@gmail.com>
2026-06-18 11:19:32 -0500

    Cambios de base de datos

--- app/database.py
+++ app/database.py
@@ -10,7 +10,10 @@ load_dotenv()
 
 # URL de conexión a la base de datos PostgreSQL
 # En un entorno de producción, DEBES usar variables de entorno para las credenciales.
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:08126172@localhost/BDSOFT")
+# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:08126172@localhost/BDSOFT")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://ruvay_h3rd_user:U5kCcpRB3I52soG2jwIG0k9km3BhI9L0@dpg-d8q0t6eq1p3s739c0kfg-a.virginia-postgres.render.com/ruvay_h3rd")
+
+engine = create_engine(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
